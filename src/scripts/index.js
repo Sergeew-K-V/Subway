439a6798c6576c5bdc4ee27ff909fbe15d8f1dd6
Kirill <89740353+Sergeew-K-V@users.noreply.github.com>
2022-03-25 18:04:42 +0300

    header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,35 +5,35 @@ const tags = {
   div: 'div',
   header: 'header',
 }
-const header = new Header(
-  `<div class="subway__block">
-<div class="subway__flex">
-  <div class="flex__top">
-    <div class="subway__logo"></div>
-    <div class="subway__img-logo"></div>
-    <div class="subway__title">Овощной</div>
-  </div>
-  <div class="flex__middle">
-    <div class="subway__link">
-      <a href="#">Соус и овощи на выбор</a>
-    </div>
-  </div>
-  <div class="flex__bottom">
-    <div class="subway__price">Цена: 110руб.</div>
-    <div class="subway__btn-block">
-      <div class="btn-block__text">Количество</div>
-      <div class="btn-block__btns-list">
-        <button class="btns-list__btn">-</button>
-        <button class="btns-list__btn">1</button>
-        <button class="btns-list__btn">+</button>
-      </div>
-    </div>
-    <div class="subway__btn-to-basket">
-      <button class="btn-to-basket__btn">В корзину</button>
-    </div>
-  </div>
-</div>
-</div>`,
-  tags.h1
-)
-console.log(header)
+// const header = new Header(
+//   `<div class="subway__block">
+// <div class="subway__flex">
+//   <div class="flex__top">
+//     <div class="subway__logo"></div>
+//     <div class="subway__img-logo"></div>
+//     <div class="subway__title">Овощной</div>
+//   </div>
+//   <div class="flex__middle">
+//     <div class="subway__link">
+//       <a href="#">Соус и овощи на выбор</a>
+//     </div>
+//   </div>
+//   <div class="flex__bottom">
+//     <div class="subway__price">Цена: 110руб.</div>
+//     <div class="subway__btn-block">
+//       <div class="btn-block__text">Количество</div>
+//       <div class="btn-block__btns-list">
+//         <button class="btns-list__btn">-</button>
+//         <button class="btns-list__btn">1</button>
+//         <button class="btns-list__btn">+</button>
+//       </div>
+//     </div>
+//     <div class="subway__btn-to-basket">
+//       <button class="btn-to-basket__btn">В корзину</button>
+//     </div>
+//   </div>
+// </div>
+// </div>`,
+//   tags.h1
+// )
+// console.log(header)
