309493304103ee753d03dc36187b967dff6100ed
Kirill <89740353+Sergeew-K-V@users.noreply.github.com>
2022-03-28 17:01:39 +0300

    stylised subway block

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,34 @@
 import Component from './Components/Component'
 import SubwayComponent from './Components/SubwayComponent'
 import Header from './Components/SubwayComponent'
-const subContent = `<div class="subway__block">
-<div class="subway__flex">
-  <div class="flex__top">
-    <div class="subway__logo"></div>
-    <div class="subway__img-logo"></div>
-    <div class="subway__title">Овощной</div>
-  </div>
-  <div class="flex__middle">
-    <div class="subway__link">
-      <a href="#">Соус и овощи на выбор</a>
+const subContent = `
+<div class="subway__block">
+  <div class="subway__flex">
+    <div class="flex__top">
+      <div class="subway__logo"></div>
+      <div class="subway__img-logo"></div>
+      <div class="subway__title">Овощной</div>
     </div>
-  </div>
-  <div class="flex__bottom">
-    <div class="subway__price">Цена: 110руб.</div>
-    <div class="subway__btn-block">
-      <div class="btn-block__text">Количество</div>
-      <div class="btn-block__btns-list">
-        <button class="btns-list__btn">-</button>
-        <button class="btns-list__btn">1</button>
-        <button class="btns-list__btn">+</button>
+    <div class="flex__middle">
+      <div class="subway__link">
+        <a href="#">Соус и овощи на выбор</a>
       </div>
     </div>
-    <div class="subway__btn-to-basket">
-      <button class="btn-to-basket__btn">В корзину</button>
+    <div class="flex__bottom">
+      <div class="subway__price">Цена: 110руб.</div>
+      <div class="subway__btn-block">
+        <div class="btn-block__text">Количество</div>
+        <div class="btn-block__btns-list">
+          <button class="btns-list__btn">-</button>
+          <button class="btns-list__btn">1</button>
+          <button class="btns-list__btn">+</button>
+        </div>
+      </div>
+      <div class="subway__btn-to-basket">
+        <button class="btn-to-basket__btn">В корзину</button>
+      </div>
     </div>
   </div>
-</div>
 </div>`
 
 const sub = new SubwayComponent(subContent)
