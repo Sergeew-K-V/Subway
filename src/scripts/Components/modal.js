dee43bc6a1bff4e0383ef94b0c1f7cdb7b785ddf
Kirill <sergeew_k@mail.ru>
2022-03-30 17:53:56 +0300

    tested modal in life mod

--- src/scripts/Components/modal.js
+++ src/scripts/Components/modal.js
@@ -1,5 +1,81 @@
 import Component from './Component'
 
+const template = `<div class="modal-overlay">
+<div class="modal">
+  <div class="container-modal">
+    <div class="modal__block">
+      <div class="modal__header">
+        <span>Выберите размер сендвича</span>
+        <div class="modal__close">
+          <span></span>
+        </div>
+      </div>
+      <div class="modal__body">
+        <div class="body__navbar">
+          <ul class="body__navbar-section">
+            <li class="navbar__item">Размер</li>
+            <li class="navbar__item">Хлеб</li>
+            <li class="navbar__item">Овощи</li>
+            <li class="navbar__item">Соусы</li>
+            <li class="navbar__item">Начинка</li>
+            <li class="navbar__item">Готово!</li>
+          </ul>
+        </div>
+        <div class="modal__btn-list">
+          <button class="modal__btn">
+            <i class="fa-solid fa-chevron-left"></i><span>Назад</span>
+          </button>
+          <button class="modal__btn">
+            <span>Вперед</span><i class="fa-solid fa-angle-right"></i>
+          </button>
+        </div>
+        <div class="modal__content" id="root-modal">
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal__footer">
+        <span>Итого: 110 руб.</span>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`
+
 export default class Modal extends Component {
   constructor(content, renderCompFunc) {
     super(content, renderCompFunc)
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,87 @@ import Component from './Components/Component'
 import SubwayComponent from './Components/SubwayComponent'
 import Header from './Components/SubwayComponent'
 import data from '../../data.json'
-
+import Modal from './Components/Modal'
+const template = `<div class="modal-overlay">
+<div class="modal">
+  <div class="container-modal">
+    <div class="modal__block">
+      <div class="modal__header">
+        <span>Выберите размер сендвича</span>
+        <div class="modal__close">
+          <span></span>
+        </div>
+      </div>
+      <div class="modal__body">
+        <div class="body__navbar">
+          <ul class="body__navbar-section">
+            <li class="navbar__item">Размер</li>
+            <li class="navbar__item">Хлеб</li>
+            <li class="navbar__item">Овощи</li>
+            <li class="navbar__item">Соусы</li>
+            <li class="navbar__item">Начинка</li>
+            <li class="navbar__item">Готово!</li>
+          </ul>
+        </div>
+        <div class="modal__btn-list">
+          <button class="modal__btn">
+            <i class="fa-solid fa-chevron-left"></i><span>Назад</span>
+          </button>
+          <button class="modal__btn">
+            <span>Вперед</span><i class="fa-solid fa-angle-right"></i>
+          </button>
+        </div>
+        <div class="modal__content" id="root-modal">
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+          <div class="modal__content-card">
+            <div class="content-card__block">
+              <div class="content-card__img">
+                <img src="/src/img/icons/sizes/1x.jpg" alt="size-15cm" />
+              </div>
+              <div class="content-card__text">15 См</div>
+              <div class="content-card__price">Цена: 110 руб.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal__footer">
+        <span>Итого: 110 руб.</span>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`
 const btnCustom = document.getElementById('btn-custom')
-btnCustom.addEventListener('click', () => {})
+btnCustom.addEventListener('click', () => {
+  const modal = new Modal(template)
+  console.log(modal)
+})
 
 // в чем идея, при инициализации компонент, давать ему уникальный индетификатор, на который мы будет ссылаться при рендере и уже отрисовывать элемент заново, в нужно блоке с этим id
 const sub = new SubwayComponent()
