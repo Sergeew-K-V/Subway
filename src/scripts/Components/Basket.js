21b3bc0a43ea717cf257af2fcc4c0af4796d72ce
Kirill <sergeew_k@mail.ru>
2022-04-05 14:31:04 +0300

    gonna fix basket

--- src/scripts/Components/Basket.js
+++ src/scripts/Components/Basket.js
@@ -3,49 +3,82 @@ import Component from './Component'
 export default class Basket extends Component {
   constructor(props) {
     super()
-    this.id = 'basket-content-root'
+    this.id = 'basket-root'
     this.price = props.price || 0
     this.name = props.name || ''
-    this.quantity = props.quantity || 0
-    this.headerBlock = `<div class="navbar__basket-block" id="${this.id}">
-    
-    </div>`
-    this.renderComp(this.headerBlock, document.getElementById('basket-root'))
+    this.quantity = props.quantity || ''
+    // this.content = `<div class="navbar__basket-block" id="${this.id}">
+    // </div>`
+    // this.renderComp(this.content, document.getElementById(this.id))
+    this.id = 'basket-content-root'
     this.getContent
-    this.renderComp(this.content, document.getElementById(this.id))
+    this.renderComp(this.content, document.getElementById('basket-root'))
   }
 
   get getContent() {
     return (this.content = `
-    <div class="basket__flex">
-      <div class="basket__header">
-        <div class="basket__icon">
-          <span class="icon-shadow"></span>
-          <i class="fa-solid fa-basket-shopping"></i>
-        </div>
-        <div class="basket__title"><span>Название</span></div>
-      </div>
-      <div class="basket__body">
-        <div class="body__top">
-          <div class="body__name"><span>Название</span></div>
-          <div class="body__quantity"><span>Количество</span></div>
-        </div>
-        <div class="body__bottom">
-          <!-- Тут будут появляться добавленнные товары -->
-          <div class="body__item">
-            <span>${this.nameValue}</span>
-            <span>${this.quantityValue}</span>
+    <div class="navbar__basket-block" id="${this.id}">
+  <div class="basket__flex">
+        <div class="basket__header">
+          <div class="basket__icon">
+            <span class="icon-shadow"></span>
+            <i class="fa-solid fa-basket-shopping"></i>
+          </div>
+          <div class="basket__title"><span>Название</span></div>
+        </div>  
+        <div class="basket__body">
+          <div class="body__top">
+            <div class="body__name"><span>Название</span></div>
+            <div class="body__quantity"><span>Количество</span></div>
+          </div>
+          <div class="body__bottom">
+            <!-- Тут будут появляться добавленнные товары -->
+            <div class="body__item">
+              <span>${this.nameValue}</span>
+              <span>${this.quantityValue}</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="basket__footer">
-        <div class="basket__total"><span>Итого: ${this.priceValue} руб.</span></div>
-      </div>
-      <div class="basket__btn">
-        <button><span>Оформить заказ</span></button>
+        <div class="basket__footer">
+          <div class="basket__total"><span>Итого: ${this.priceValue} руб.</span></div>
+        </div>
+        <div class="basket__btn">
+          <button><span>Оформить заказ</span></button>
+        </div>
       </div>
     </div>`)
   }
+  // get getContent() {
+  //   return (this.content = `
+  //   <div class="basket__flex">
+  //     <div class="basket__header">
+  //       <div class="basket__icon">
+  //         <span class="icon-shadow"></span>
+  //         <i class="fa-solid fa-basket-shopping"></i>
+  //       </div>
+  //       <div class="basket__title"><span>Название</span></div>
+  //     </div>
+  //     <div class="basket__body">
+  //       <div class="body__top">
+  //         <div class="body__name"><span>Название</span></div>
+  //         <div class="body__quantity"><span>Количество</span></div>
+  //       </div>
+  //       <div class="body__bottom">
+  //         <!-- Тут будут появляться добавленнные товары -->
+  //         <div class="body__item">
+  //           <span>${this.nameValue}</span>
+  //           <span>${this.quantityValue}</span>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div class="basket__footer">
+  //       <div class="basket__total"><span>Итого: ${this.priceValue} руб.</span></div>
+  //     </div>
+  //     <div class="basket__btn">
+  //       <button><span>Оформить заказ</span></button>
+  //     </div>
+  //   </div>`)
+  // }
 
   get priceValue() {
     return this.price
