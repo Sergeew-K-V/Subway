03b2cec82b55383c5089ce5ddc45da9441218876
Kirill <89740353+Sergeew-K-V@users.noreply.github.com>
2022-04-18 13:27:54 +0300

    go no office

--- src/scripts/Components/Main.js
+++ src/scripts/Components/Main.js
@@ -4,26 +4,81 @@ import Modal from './Modal'
 import Basket from './Basket'
 import Component from './Component'
 import Menu from './Menu'
+import { menuProxy } from './Menu'
+import EventEmitter from '../EventEmitter'
 
 class Main extends Component {
   constructor(props) {
     super()
-    this.init()
-  }
-  init() {
-    const basket = new Basket({})
-    // const menu = new Menu()
-    ////Первая часть - инициализация элементов
-    const sandwiches = data.menu.filter((el) => el.category === 'sandwiches')
-    sandwiches.map((el) => {
-      const subTemp = new SubwayComponent(el, basket)
-      subTemp.listeners()
-      return subTemp
-    })
-    const menu = new Menu(data)
+    this.content = `<div class="container-big">
+                      <div class="main__block">
+                        <div class="navbar__block">
+                          <div class="navbar__top">
+                            <div class="navbar__menu-block" id="menu__root"></div>
+                          </div>
+                          <div class="navbar__middle">
+                            <div class="navbar__btn-custom">
+                              <button id="btn-custom"><span>Собрать свой</span></button>
+                            </div>
+                          </div>
+                          <div class="navbar__bottom" id="basket-root">
+                            <!-- Here is a basket -->
+                          </div>
+                        </div>
+                        <div class="main__content">
+                          <div class="container-content">
+                            <div id="root" class="main__flex"></div>
+                          </div>
+                          <!-- hello -->
+                        </div>
+                      </div>
+                    </div>`
+    this.id = 'root-main'
+    this.renderComp(this.content, document.getElementById(this.id))
   }
 }
 const main = new Main()
+menuProxy.category = 'sandwiches'
+const basket = new Basket({})
+const sandwiches = data.menu.filter((el) => el.category === 'sandwiches')
+sandwiches.map((el) => {
+  const subTemp = new SubwayComponent(el, basket)
+  subTemp.listeners()
+  return subTemp
+})
+const emitter = new EventEmitter()
+console.log(emitter)
+
+let lastMenuItemId = null // last menu item if was selected
+emitter.subscribe('menu__item pressed', () => {
+  console.log('emmiter')
+  const navbarMenu = document.querySelector('.navbar__menu')
+  navbarMenu.addEventListener('click', (e) => {
+    if (e.target.closest('.menu__item')) {
+      const categoryId = e.target.closest('.menu__item').id
+      const currMenuItem = document.getElementById(categoryId)
+      currMenuItem.classList.add('selected')
+      if (lastMenuItemId === categoryId) {
+        alert('Already opened')
+      } else {
+        if (lastMenuItemId !== null) {
+          const lastMenuItem = document.getElementById(lastMenuItemId)
+          lastMenuItem.classList.remove('selected')
+        }
+        lastMenuItemId = categoryId
+        switch (categoryId) {
+          case 'sandwiches':
+            menuProxy.category = 'sandwiches'
+            break
+          case 'shaurma':
+            menuProxy.category = 'shaurma'
+            break
+        }
+      }
+    }
+  })
+})
+emitter.emit('menu__item pressed')
 // const selected = 'sandwiches';
 
 // setCategory(c) {
@@ -36,32 +91,32 @@ const main = new Main()
 //animation and logic for transition of 'meun__item'
 
 ////Модальное окно
-const btnCustom = document.getElementById('btn-custom')
-let idChanged = false
-btnCustom.addEventListener('click', () => {
-  const dataForModal = {
-    sizes: data.sizes,
-    breads: data.breads,
-    vegetables: data.vegetables,
-    sauces: data.sauces,
-    fillings: data.fillings,
-  }
-  //Changing ID for correct seacrh of curr Element
-  if (!idChanged) {
-    for (let key in dataForModal) {
-      for (let secKey in dataForModal[key]) {
-        dataForModal[key][secKey].id = 'modal-' + dataForModal[key][secKey].id
-        // console.log(dataForModal[key][secKey].id)
-      }
-      idChanged = true
-    }
-  }
-  // basket not needed
-  const modal = new Modal(dataForModal, basket)
-  modal.listenerForBtnBack()
-  //Закрытие модального окна
-  const modalClose = document.querySelector('.modal__close')
-  modalClose.addEventListener('click', () => {
-    modal.destroyModal()
-  })
-})
+// const btnCustom = document.getElementById('btn-custom')
+// let idChanged = false
+// btnCustom.addEventListener('click', () => {
+//   const dataForModal = {
+//     sizes: data.sizes,
+//     breads: data.breads,
+//     vegetables: data.vegetables,
+//     sauces: data.sauces,
+//     fillings: data.fillings,
+//   }
+//   //Changing ID for correct seacrh of curr Element
+//   if (!idChanged) {
+//     for (let key in dataForModal) {
+//       for (let secKey in dataForModal[key]) {
+//         dataForModal[key][secKey].id = 'modal-' + dataForModal[key][secKey].id
+//         // console.log(dataForModal[key][secKey].id)
+//       }
+//       idChanged = true
+//     }
+//   }
+//   // basket not needed
+//   const modal = new Modal(dataForModal, basket)
+//   modal.listenerForBtnBack()
+//   //Закрытие модального окна
+//   const modalClose = document.querySelector('.modal__close')
+//   modalClose.addEventListener('click', () => {
+//     modal.destroyModal()
+//   })
+// })
--- src/scripts/Components/Menu.js
+++ src/scripts/Components/Menu.js
@@ -1,11 +1,12 @@
 import Component from './Component'
-export default class Menu extends Component {
+import data from '../../../data.json'
+class Menu extends Component {
   constructor(props) {
     super()
-    this.content = `<ul class="navbar__menu">
+    this.content = `<ul class="navbar__menu" id="menu__subRoot">
                         <li class="menu__item" id="pizza"><a href="#">Блины</a></li>
                         <li class="menu__item" id="shaurma"><a href="#">Шаурма</a></li>
-                        <li class="menu__item selected" id="sandwiches"><a href="#">Сендвичи</a></li>
+                        <li class="menu__item" id="sandwiches"><a href="#">Сендвичи</a></li>
                         <li class="menu__item" id="burgers"><a href="#">Бургеры</a></li>
                         <li class="menu__item" id="chicken"><a href="#">Курица & Картофель</a></li>
                         <li class="menu__item" id="salads"><a href="#">Тортилья & Салаты</a></li>
@@ -13,67 +14,21 @@ export default class Menu extends Component {
                     </ul>`
     this.id = 'menu__root'
     this.category = 'sandwiches'
-    this.renderComp(this.content, document.getElementById(this.id))
-    this.listenerChangingCategory(props)
-  }
-  listenerChangingCategory(data) {
-    let lastMenuItemId = null // last menu item if was selected
-    const navbarMenu = document.querySelector('.navbar__menu')
-    navbarMenu.addEventListener('click', (e) => {
-      if (e.target.closest('.menu__item')) {
-        const categoryId = e.target.closest('.menu__item').id
-        const currMenuItem = document.getElementById(categoryId)
-        currMenuItem.classList.add('selected')
-        if (lastMenuItemId === categoryId) {
-        } else {
-          if (lastMenuItemId !== null) {
-          }
-          lastMenuItemId = categoryId
-          switch (categoryId) {
-            case 'sandwiches':
-              createMainBlock()
-              const subways = data.menu.filter((el) => el.category === categoryId)
-              const subwaysArray = subways.map((el) => {
-                const subTemp = new SubwayComponent(el, basket)
-                subTemp.listeners()
-                return subTemp
-              })
-
-              break
-            case 'shaurma':
-              createMainBlock()
-              const shaurmas = data.menu.filter((el) => el.category === categoryId)
-              shaurmas.map((el) => {
-                const shaurmaTemp = new SubwayComponent(el, basket)
-                shaurmaTemp.listeners()
-                return shaurmaTemp
-              })
-              console.log('123123')
-              break
-          }
-        }
-      }
-    })
-  }
-  set setCategory(value) {
-    this.category = value
+    this.firstInit = true
   }
 }
 const menuLittle = new Menu()
-const menuProxy = new Proxy(menuLittle, {
+export const menuProxy = new Proxy(menuLittle, {
   set(target, prop, value) {
-    debugger
     target[prop] = value
+    if (!target.firstInit) {
+      target.destroy('menu__subRoot')
+    } else {
+      target.firstInit = false
+    }
+
     target.renderComp(target.content, document.getElementById(target.id))
     console.log("it's proxy render")
+    return true
   },
 })
-
-function createMainBlock() {
-  const container = document.getElementById('root').remove()
-  const position = document.querySelector('.container-content')
-  const newRoot = document.createElement('div')
-  newRoot.classList.add('main__flex')
-  newRoot.id = 'root'
-  position.insertAdjacentElement('beforeend', newRoot)
-}
