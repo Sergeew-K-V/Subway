c1ca2a570a188d46059bb36b5c62bbc753d72ad3
Kirill <sergeew_k@mail.ru>
2022-04-19 16:44:19 +0300

    made basket price and content render - working +

--- src/scripts/Components/Basket.js
+++ src/scripts/Components/Basket.js
@@ -3,92 +3,66 @@ import Component from './Component'
 export default class Basket extends Component {
   constructor(props = {}) {
     super()
-
-    // this.data = new Proxy(
-    //   {
-    //     id,
-    //     arrayOfGoods,
-    //   },
-    //   () => {
-    // this.render()
-    //   }
-    // )
-
-    // eventEmitter.subscribe('addToBasket', (product) => {
-    //   this.data.arrayOfGoods = [...this.data.arrayOfGoods, product]
-    // })
-
     this.arrayOfGoods = []
     this.id = 'basket-root'
-    this.price = props.price || 0
-    this.name = props.name || ''
-    this.quantity = props.quantity || ''
-    this.content = `
-    <div class="navbar__basket-block" >
-      <div class="basket__flex">
-        <div class="basket__header">
-          <div class="basket__icon">
-            <span class="icon-shadow"></span>
-            <i class="fa-solid fa-basket-shopping"></i>
-          </div>
-          <div class="basket__title"><span>Название</span></div>
-        </div>  
-        <div class="basket__body" id='place-for-body-item'>
-          <div class="body__top">
-            <div class="body__name"><span>Название</span></div>
-            <div class="body__quantity"><span>Количество</span></div>
-          </div>
-            <!-- Тут будут появляться добавленнные товары -->
-        </div>
-        <div class="basket__footer" id="place-price">
-          <div class="basket__total"><span>Итого: ${this.priceValue} руб.</span></div>
-        </div>
-        <div class="basket__btn" >
-          <button><span>Оформить заказ</span></button>
-        </div>
-      </div>
-    </div>`
-
-    this.renderComp(this.content, document.getElementById(this.id))
-    this.id = 'place-for-body-item'
-    this.basketRender()
+    this.dataBasket = new Proxy(
+      {
+        price: 0,
+      },
+      {
+        set: (target, key, value) => {
+          console.log('Proxy from basket - SETTER')
+          target.price = getTotalPrice(this.arrayOfGoods, this.dataBasket.price)
+          this.renderComp(this.getContent, document.getElementById(this.id))
+          this.listener()
+          return true
+        },
+        get: (target, key) => {
+          console.log("it's prxy of product - GETTER(No re-render)")
+          return target.price
+        },
+      }
+    )
+    this.renderComp(this.getContent, document.getElementById(this.id))
     this.listener()
   }
-  get priceValue() {
-    return this.price
-  }
-  set priceValue(value) {
-    this.price = value
-    this.reRenderPrice()
-  }
-  get nameValue() {
-    return this.name
-  }
-  set nameValue(value) {
-    this.name = value
-  }
-  get quantityValue() {
-    return this.quantity
-  }
-  set quantityValue(value) {
-    this.quantity = value
-  }
-  basketRender() {
-    this.content = `<div class="body__bottom" id='array__wrapper'>
-    </div>`
-    this.id = 'place-for-body-item'
-    this.renderComp(this.content, document.getElementById(this.id))
-
-    this.arrayOfGoods.map((el) => {
-      this.content = `
-      <div class="body__item" id="${el.id}">
-        <span class="body__item_left">${el.name}</span>
-        <span class="body__item_right">${el.quantity} <i class="fa-solid fa-trash-can"></i></span>
+  get getContent() {
+    return (this.content = `<div class="navbar__basket-block" id="basket-subRoot">
+    <div class="basket__flex">
+      <div class="basket__header">
+        <div class="basket__icon">
+          <span class="icon-shadow"></span>
+          <i class="fa-solid fa-basket-shopping"></i>
+        </div>
+        <div class="basket__title"><span>Название</span></div>
+      </div>  
+      <div class="basket__body" id='place-for-body-item'>
+        <div class="body__top">
+          <div class="body__name"><span>Название</span></div>
+          <div class="body__quantity"><span>Количество</span></div>
+        </div>
+        <div class="body__bottom" id='array__wrapper'>
+          ${this.arrayOfGoods
+            .map((el) => {
+              return `
+              <div class="body__item" id="${el.id}">
+                <span class="body__item_left">${el.name}</span>
+                <span class="body__item_right">${el.quantity} <i class="fa-solid fa-trash-can"></i></span>
+              </div>
+              `
+            })
+            .join('')}
+        </div>
+          <!-- Тут будут появляться добавленнные товары -->
       </div>
-      `
-      this.id = 'array__wrapper'
-      this.renderComp(this.content, document.getElementById(this.id))
-    })
+      <div class="basket__footer" id="place-price">
+        <div class="basket__total"><span>Итого: ${this.dataBasket.price} руб.</span></div>
+      </div>
+      <div class="basket__btn" >
+        <button><span>Оформить заказ</span></button>
+      </div>
+    </div>
+  </div>`)
   }
   listener() {
     const basketBody = document.getElementById('place-for-body-item')
@@ -99,31 +73,17 @@ export default class Basket extends Component {
       }
     })
   }
-  reRenderPrice() {
-    this.content = `<div class="basket__total"><span>Итого: ${this.priceValue} руб.</span></div>`
-    document.querySelector('.basket__total').remove()
-    this.renderComp(this.content, document.getElementById('place-price'))
-  }
-  destroy(id = 'array__wrapper') {
-    const destroyPoint = document.getElementById(id)
-    destroyPoint.remove()
-  }
   addItem(data) {
     getArrayOfBasket(this.arrayOfGoods, data.id, data.name, data.quantity, data.price)
-    this.priceValue = getTotalPrice(this.arrayOfGoods, this.priceValue)
-    this.destroy()
-    this.basketRender()
-    // console.log('arrayofgoods', this.arrayOfGoods)
+    this.destroy('basket-subRoot')
+    this.dataBasket.price = getTotalPrice(this.arrayOfGoods, this.dataBasket.price)
   }
 
   removeItem(sendedId) {
     const removingItem = this.arrayOfGoods.find((el) => el.id === sendedId)
     this.arrayOfGoods = this.arrayOfGoods.filter((el) => el != removingItem)
-    this.destroy()
-    this.basketRender()
-    this.priceValue -= removingItem.price * removingItem.quantity
-    this.reRenderPrice()
-    // console.log('removing item from basket', removingItem)
+    this.destroy('basket-subRoot')
+    this.dataBasket.price = getTotalPrice(this.arrayOfGoods, this.dataBasket.price)
   }
 }
 function getTotalPrice(array, price) {
